7e978135a76a5dc43142d7002c73a1167a8966a1
Ester Santos <ester03lais@gmail.com>
2026-02-20 21:33:13 -0300

    Add files via upload

--- Catalogo Isabelle.M/JS/main.js
+++ Catalogo Isabelle.M/JS/main.js
@@ -1,66 +1,111 @@
-const API_KEY = "9ed4d53db23dc98cfda64348b807b218";
-const BASE_URL = "https://api.themoviedb.otg/3";
+const API_KEY = "a3fda9b9d1d0aaee95df37313c16684e"; 
+const BASE_URL = "https://api.themoviedb.org/3";
 const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
 
 const campoPesquisa = document.getElementById("campoPesquisa");
 const botaoPesquisa = document.getElementById("botaoPesquisa"); 
 const filmesGrid = document.getElementById("filmesGrid");
+const inicio = document.getElementById("inicio");
+const filmes = document.getElementById("filmes");
+const series = document.getElementById("series");
 
-async function requisicaoFilmes(url) {
+
+async function requisicaoURL(url) {
     try {
         const response = await fetch(url);
+
         if (!response.ok) {
             throw new Error("Erro na requisição");
         }
+
         const data = await response.json();
-        renderizarFilmes(data.results); 
+        renderizarMidia(data.results);
+
     } catch (error) {
-        console.error("Error", error);
+        console.error("Erro:", error);
         filmesGrid.innerHTML = "<p>Erro ao carregar filmes.</p>";
     }
 }
 
-function buscaFilme(){ 
+function pesquisaGeral(){ 
     const informacao = campoPesquisa.value.trim(); 
-    if   (informacao === "") { 
-        window.location.reload (); 
+
+    if (informacao === "") { 
+        carregarTendenciaGeral();
         return;
     } 
-    console.log("Pesquisando por:", informacao); 
-    const url = `$(BASE_URL)/search/movie?api_key=${API_KEY}
-    &query=${encodeURIComponent(informacao)}&language=pt-BR`;
-    requisicaoFilmes(url);
+
+    const url = `${BASE_URL}/search/multi?api_key=${API_KEY}&query=${encodeURIComponent(informacao)}&language=pt-BR`;
+
+    requisicaoURL(url);
     campoPesquisa.value = "";
 } 
 
-function renderizarFilmes(filmes) {
+function renderizarMidia(filmes) {
     filmesGrid.innerHTML = "";
-    if(!filmes || filmes.length === 0){
+
+    if (!filmes || filmes.length === 0){
         filmesGrid.innerHTML = "<p>Nenhum filme encontrado.</p>";
         return;
     }
-    filmes.forEach(filme =>) {
+
+    filmes.forEach(filme => {
+
         const card = document.createElement("div");
         card.classList.add("card");
+
         const imagem = filme.poster_path
             ? IMAGE_URL + filme.poster_path
-            : "" ;
-        card.innerHTML = `
-            <img src="${imagem}" alt="${filme.title}">
-            <h3>${filme.title}</h3>
-            <p>${filme.vote_average}
-                ${filme.overview}
-                ${filme.genre_ids}
-                ${filme.release_date}
-            </p>
-        `;
+            : "";
+
+            if(filme.title) {
+                card.innerHTML = `
+                <img src="${imagem}" alt="${filme.title}">
+                <h3>${filme.title}</h3>
+                <p><strong>Nota:</strong> ${filme.vote_average}</p>
+                <p><strong>Lançamento:</strong> ${filme.release_date}</p>
+                <p>${filme.overview}</p>
+                `;
+                
+            }else{
+                card.innerHTML = `
+                <img src="${imagem}" alt="${filme.name}">
+                <h3>${filme.name}</h3>
+                <p><strong>Nota:</strong> ${filme.vote_average}</p>
+                <p><strong>Lançamento:</strong> ${filme.release_date}</p>
+                <p>${filme.overview}</p>
+                `;
+            }
+
         filmesGrid.appendChild(card);
     });
+} 
+ 
+function carregarTendenciaGeral(){ 
+    const url = `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=pt-BR`; 
+    requisicaoURL(url);
 }
 
-botaoPesquisa.addEventListener("click", buscaFilme); 
+function buscaFilme(){ 
+    const url = `${BASE_URL}/trending/movie/week?api_key=${API_KEY}&language=pt-BR`; 
+    requisicaoURL(url);
+}
+
+function buscaSerie(){ 
+    const url = `${BASE_URL}/trending/tv/week?api_key=${API_KEY}&language=pt-BR`; 
+    requisicaoURL(url);
+}
+
+document.addEventListener("DOMContentLoaded", carregarTendenciaGeral);
+
+botaoPesquisa.addEventListener("click", pesquisaGeral);
+
 campoPesquisa.addEventListener("keydown", function (event) { 
     if (event.key === "Enter") {
-        buscaFilme (); 
+        pesquisaGeral(); 
     }    
 });
+document.addEventListener("DOMContentLoaded", carregarTendenciasGeral); 
+inicio.addEventListener("click", carregarTendenciaGeral); 
+filmes.addEventListener("click", buscaFilme); 
+series.addEventListener("click", buscaSerie);
