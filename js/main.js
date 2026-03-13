03b8caf981f8a2d2fc42449df10bccd599678d74
Ester Santos <ester03lais@gmail.com>
2026-03-13 19:38:44 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ function renderizarMidia(filmes) {
     filmesGrid.innerHTML = "";
 
     if (!filmes || filmes.length === 0){
-        filmesGrid.innerHTML = "<p>Nenhum filme encontrado.</p>";
+        filmesGrid.innerHTML = "<p>Nenhum filme ou série encontrado.</p>";
         return;
     }
 
@@ -83,9 +83,14 @@ function renderizarMidia(filmes) {
                 <p>${filme.overview}</p>
                 `;
             } 
+            // ATENÇÃO AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
 
             card.addEventListener("click", () => { 
+                // VERIFICAR COM A PROFESSORA, POIS CONSERTOU MAS FOI COM BASE NO GPF
+                // window.location.href = `pages/detalhe.html?id=${filme.id}&type=${filme.media_type}`;
+                // ESSE CODIGO ACIMA É O QUE JA ESTAVA NO CODIGO E ESTAVA GERANDO ERRO
                 window.location.href = `pages/detalhe.html?id=${filme.id}&type=${filme.title ? "movie" : "tv"}`;
+                // ESSE CODIGO ACIMA PRECISA VERIFICAR COM A PROFESSORA, POIS FOI O GPT QUE REPORTOU QUE FALTAVA INFORMAÇÃO. PRIMEIRO REPORTAR O ERRO E VER SE É SO NO NOSSO, SE TEM RESOLUÇÃO
             });
 
 
@@ -192,4 +197,3 @@ document.addEventListener("DOMContentLoaded", () => {
     carregarAnos();
 
 });
-
